afd522122f53077642f2339e15cff6e250315d10
William <williamwijaya.mail@gmail.com>
2022-05-19 17:04:27 -0700

    Axios and React, npm install

--- react-albums/src/components/LiveSearch.js
+++ react-albums/src/components/LiveSearch.js
@@ -1,11 +1,28 @@
-import React, { Fragment, useState } from "react";
+import React, { Fragment, useState, useEffect } from "react";
 
 import SearchBar from "components/SearchBar";
 import Results from "components/Results";
+import axios from "axios";
 
 export default function LiveSearch(props) {
   const [term, setTerm] = useState("");
   const [results, setResults] = useState([]);
+
+  useEffect (() => {
+    // const testURL = `https://search-itunes.vercel.app/?term=$%7Bterm%7D&country=CA&media=music&entity=album&attribute=artistTerm`;
+    const testURL = `https://search-itunes.vercel.app/?term=${term}&country=CA&media=music&entity=album&attribute=artistTerm`
+    
+    axios
+      .get(testURL)
+      .then(response => {
+         
+      console.log("response", response.data.results);
+
+      setResults([...response.data.results])
+      });
+      
+  }, [term]);
+
 
   return (
     <Fragment>
